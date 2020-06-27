ccf7e2f99e69c8f3a33d560635ca1c402f2e1795
wendy liu <40872440+wliu11@users.noreply.github.com>
2020-06-26 22:10:57 -0700

    Updated with comments

--- train.py
+++ train.py
@@ -1,21 +1,31 @@
 from .models import ClassificationLoss, model_factory, save_model
-from .utils import accuracy, load_data
+from .utils import load_data
 import torch.optim as optim
+import argparse
 
+"""
+Path to reach the directory with training data
+"""
 TRAIN_PATH = "data/train"
-VALID_PATH = "data/valid"
 
+"""
+Adjustable parameters 
+"""
+num_epochs = 35
+lr = 0.000005
 
+"""
+Takes in the training data and uses stochastic gradient descent as an optimizer,
+makes a prediction and then calculates the overall loss from the predicted and actual class.
+"""
 def train(args):
 
     model = model_factory[args.model]()
-
     train_data = load_data(TRAIN_PATH)
-    
-    optimizer = optim.SGD(model.parameters(), lr=0.000005, )
+    optimizer = optim.SGD(model.parameters(), lr)
     loss_function = ClassificationLoss()
     
-    for epoch in range(35):
+    for epoch in range(num_epochs):
         print("epoch: " + str(epoch))
         for images, labels in train_data:
             predictions = model(images)
@@ -28,12 +38,7 @@ def train(args):
 
 
 if __name__ == '__main__':
-    import argparse
-
     parser = argparse.ArgumentParser()
-
     parser.add_argument('-m', '--model', choices=['linear', 'mlp'], default='linear')
-
     args = parser.parse_args()
-
     train(args)
